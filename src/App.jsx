040b194ac4a47ffec403f30771ca67fe309cc4a0
HUANG YI NING-CODE <a0960102890@gmail.com>
2025-05-13 16:49:06 +0800

    0513

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './pages/About'
+import Menu from './pages/Menu'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
         <main className="main-content">
           <Routes>
             <Route path="/about" element={<About />} />
+            <Route path="/menu" element={<Menu />} />
           </Routes>
         </main>
       </BrowserRouter>
---
+++ src/pages/Menu.jsx
@@ -0,0 +1,76 @@
+import '../App.scss/css/Style.min.css'
+// import HeaderPlain from '../components/HeaderPlain'
+// import FooterBg from '../components/FooterBg'
+
+export default function About() {
+    return (
+        <div className="menu">
+            {/* <HeaderPlain/> */}
+
+            <div>
+                <nav className="navigation">
+                    <span className="closebtn"><img src="./images/icon_closebgrbtn.svg" alt=""/></span>
+                    <span className="closebtn"><img src="../public/image/icon/" alt=""/></span>
+                    <div className="menu">
+                        <h2>About us</h2>
+                        <ul>
+                            <li><a href="../Aboutus.html">品牌故事</a></li>
+                            <li><a href="#">品質保證</a></li>
+                        </ul>
+                    </div>
+
+                    <div className="menu">
+                        <h2>Discoveries</h2>
+                        <ul>
+                            <li><a href="../Knowledge1.html">使用與淨化指南</a></li>
+                            <li><a href="../Knowledge2.html">水晶與基礎知識</a></li>
+                        </ul>
+                    </div>
+                    <div className="menu">
+                        <h2>Collections</h2>
+                        <ul>
+                            <li><a href="#">澄語之藍</a></li>
+                            <li><a href="#">秘境探光</a></li>
+                            <li><a href="#">四季琉波</a></li>
+                            <li><a href="#">微醺晶釀</a></li>
+                        </ul>
+                    </div>
+                    <div className="menu">
+                        <h2>Customized</h2>
+                        <div className="menu-child">
+                            <ul>
+                                <li><a href="../Jasmine/numtest/numtest-1.html">生命靈數測驗</a></li>
+                                <li><a href="../阿啾/html/customization-1.html">開始客製</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="menu-last">
+                        <div className="menu-last1">
+                            <h2>Membership</h2>
+                            <ul>
+                                <li><a href="#">註冊/登入</a></li>
+                                <li>會員中心</li>
+                                <li><a href="">訂單查詢</a></li>
+                                <li>收藏清單</li>
+                            </ul>
+                        </div>
+                        <div className="menu-last2">
+                            <h3>FOLLOW US</h3>
+                            <span className="scicon-box">
+                                <div className="scicon"><img src="../images/icon_line.svg" alt=""/></div>
+                                <div className="scicon"><img src="./images/sns-ig-state-default.svg" alt=""/></div>
+                                <div className="scicon"><img src="./images/sns-fb-state-default.svg" alt=""/></div>
+                            </span>
+                        </div>
+                    </div>
+                </nav>
+                {/* <FooterBg /> */}
+            </div>
+            </div>
+    )}
+            
+            
+
+
+
+
