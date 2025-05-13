85d187d34b7cf0df304f067431fc6fd0aefcac3c
ktlinktlinherstudio <catdogorz520@gmail.com>
2025-05-13 17:34:25 +0800

    111

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './pages/About'
 import Menu from './pages/Menu'
+import KnowledgeCrystal from './pages/KnowledgeCrystal'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
           <Routes>
             <Route path="/about" element={<About />} />
             <Route path="/menu" element={<Menu />} />
+            <Route path="/KnowledgeCrystal" element={<KnowledgeCrystal />} />
           </Routes>
         </main>
       </BrowserRouter>
