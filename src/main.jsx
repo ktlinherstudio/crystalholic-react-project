70e843e22ab4ab38a62301a29af416dc052191b6
JOHsu0303 <tinahsu0224@gmail.com>
2025-05-15 10:54:41 +0800

    0515-2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
-import App from './pages/Customize/Customize1'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
