13d41f8867103e0dd66f7a16cd6a713f75b12b81
JasmineHu2025 <JasmineHu2025>
2025-05-15 21:14:06 +0800

    0515-6j

---
+++ src/components/NavBarLight.jsx
@@ -0,0 +1,20 @@
+import './NavBarLight.css'
+
+export default function NavBarLight() {
+  return (
+    <header id="navbar">
+      <a className="logo" href="#">
+        <img src="/images/S-NavBar/logotext_nav2.svg" alt=""/>
+      </a>
+      <nav className="navigation">
+        {/* icon btn */}
+        <ul className="icon">
+          <li><a href="#"><img src="/images/S-NavBar/navicon_search.svg" alt="Search" /></a></li>
+          <li><a href="#"><img src="/images/S-NavBar/navicon_cart.svg" alt="Cart" /></a></li>
+          <li><a href="#"><img src="/images/S-NavBar/navicon_member.svg" alt="Member" /></a></li>
+          <li><a href="#"><img src="/images/S-NavBar/navicon_bgrbtn.svg" alt="Menu" /></a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
