0b38417c00073629cc0d795a9424cba4dcb5c522
ktlinherstudio <catdogorz520@gmail.com>
2025-05-16 13:24:37 +0800

    0516-3v

--- src/components/NavBarLight.jsx
+++ src/components/NavBarLight.jsx
@@ -1,20 +1,36 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import LoginRegisterModal from './LoginRegisterModal';
 import './NavBarLight.css'
 
 export default function NavBarLight() {
+  const [showAuthModal, setShowAuthModal] = useState(false);
   return (
-    <header id="navbar">
-      <a className="logo" href="#">
-        <img src="/images/S-NavBar/logotext_nav2.svg" alt=""/>
-      </a>
-      <nav className="navigation">
-        {/* icon btn */}
-        <ul className="icon">
-          <li><a href="#"><img src="/images/S-NavBar/navicon_search.svg" alt="Search" /></a></li>
-          <li><a href="#"><img src="/images/S-NavBar/navicon_cart.svg" alt="Cart" /></a></li>
-          <li><a href="#"><img src="/images/S-NavBar/navicon_member.svg" alt="Member" /></a></li>
-          <li><a href="#"><img src="/images/S-NavBar/navicon_bgrbtn.svg" alt="Menu" /></a></li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="navbar">
+        <a className="logo" href="#">
+          <img src="/images/S-NavBar/logotext_nav2.svg" alt="" />
+        </a>
+        <nav className="navigation">
+          {/* icon btn */}
+          <ul className="icon">
+            <li><a href="#"><img src="/images/S-NavBar/navicon_search.svg" alt="Search" /></a></li>
+            <li><a href="#"><img src="/images/S-NavBar/navicon_cart.svg" alt="Cart" /></a></li>
+            <li>
+              {/* 按下會員 icon 彈出視窗 */}
+              <button onClick={() => setShowAuthModal(true)} className="member-btn">
+                <img src="/images/S-NavBar/navicon_member.svg" alt="Member" />
+              </button>
+            </li>
+            <li><a href="#"><img src="/images/S-NavBar/navicon_bgrbtn.svg" alt="Menu" /></a></li>
+          </ul>
+        </nav>
+      </header>
+      {/* 彈跳視窗元件 */}
+      <LoginRegisterModal
+        isOpen={showAuthModal}
+        onClose={() => setShowAuthModal(false)}
+      />
+    </>
   )
 }
