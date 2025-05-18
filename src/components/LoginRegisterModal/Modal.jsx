236cecdf7917dd1827f018007f8504fe95e6db8a
ktlinktlinherstudio <catdogorz520@gmail.com>
2025-05-18 19:42:04 +0800

    0518-19v

--- src/components/LoginRegisterModal/Modal.jsx
+++ src/components/LoginRegisterModal/Modal.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from 'react';
+import './Modal.css';
+
 export default function Modal({ isOpen, onClose, children }) {
+  useEffect(() => {
+    if (isOpen) {
+      // 鎖定背景 scroll，但不會動到 position 或 top
+      document.body.style.overflow = 'hidden';
+
+      return () => {
+        document.body.style.overflow = '';
+      };
+    }
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
