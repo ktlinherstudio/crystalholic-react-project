1e6e38357632fd8f59f4a2ee0b1833f5e006c70a
JasmineHu2025 <JasmineHu2025>
2025-05-23 22:28:50 +0800

    0523-3

--- src/pages/NumTest/NumTest1.jsx
+++ src/pages/NumTest/NumTest1.jsx
@@ -5,20 +5,20 @@ export default function NumTest1(){
     return(
         <>
             <NavBarDark />
-            <main>
+            <main className="numtest1">
                 <section className="num1-numtest">
                     {/* 第一個標題 */}
-                    <div id="num1-title-1">
+                    <div className="num1-title-1">
                         <img className="num1-decorate-left" src="/images/Numtest/decorate-left.svg" alt="左裝飾" />
                         <h3 className="num1-decorate-title">探索適合的水晶</h3>
                         <img className="num1-decorate-right" src="/images/Numtest/decorate-right.svg" alt="右裝飾" />
                     </div>
 
                     {/* 第二個標題 */}
-                    <h1 id="num1-title-2">生命靈數測驗</h1>
+                    <h1 className="num1-title-2">生命靈數測驗</h1>
 
                     {/* 按鈕 */}
-                    <button id="num1-button">START!</button>
+                    <button className="num1-button">START!</button>
                 </section>
             </main>
         </>
