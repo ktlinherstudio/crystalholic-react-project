b5fa99e837e0e5a34ee2d835382230dccb9008be
ktlinktlinherstudio <catdogorz520@gmail.com>
2025-05-25 22:23:14 +0800

    0525-3v

--- src/components/MenuOverlay.jsx
+++ src/components/MenuOverlay.jsx
@@ -1,12 +1,15 @@
 import './MenuOverlay.css';
 import BgDarkBlock from './BgDarkBlock';
 import { useUI } from '../context/UIContext';
+import { Link } from 'react-router-dom';
+
 
 export default function MenuOverlay() {
   const { isMenuOpen, closeMenu } = useUI();
 
   return (
     <div className={`menu-overlay ${isMenuOpen ? 'open' : ''}`}>
+      {/* <div className="menu-backdrop"></div> */}
       <BgDarkBlock />
       <div className="menu-panel">
         <nav className="menu-navigation">
@@ -14,39 +17,46 @@ export default function MenuOverlay() {
             <img src="/images/S-NavBar/navicon_closebgrbtn.svg" alt="close" />
           </span>
 
-          <div className="menu-column">
-            <h2>About us</h2>
-            <ul>
-              <li><a href="../Aboutus.html">品牌故事</a></li>
-              <li><a href="#">品質保證</a></li>
-            </ul>
-          </div>
-
-          <div className="menu-column">
-            <h2>Discoveries</h2>
-            <ul>
-              <li><a href="../Knowledge1.html">使用與淨化指南</a></li>
-              <li><a href="../Knowledge2.html">水晶與基礎知識</a></li>
-            </ul>
-          </div>
-
-          <div className="menu-column">
-            <h2>Collections</h2>
-            <ul>
-              <li><a href="#">澄語之藍</a></li>
-              <li><a href="#">秘境探光</a></li>
-              <li><a href="#">四季琉波</a></li>
-              <li><a href="#">微醺晶釀</a></li>
-            </ul>
-          </div>
-
-          <div className="menu-column">
-            <h2>Customized</h2>
-            <div className="menu-child">
+          <div className="menu-columns-wrapper">
+            <div className="menu-column">
+              <h2>About us</h2>
               <ul>
-                <li><a href="../Jasmine/numtest/numtest-1.html">生命靈數測驗</a></li>
-                <li><a href="../阿啾/html/customization-1.html">開始客製</a></li>
+                <li><Link to="/about" className="hover-link">
+                  <p>品牌故事</p>
+                </Link></li>
               </ul>
+            </div>
+
+            <div className="menu-column">
+              <h2>Discoveries</h2>
+              <ul>
+                <li><Link to="/KnowledgeCleanse" className="hover-link">
+                  <p>使用與淨化指南</p>
+                </Link></li>
+                <li><Link to="/KnowledgeCrystal" className="hover-link">
+                  <p>水晶小百科</p>
+                </Link></li>
+              </ul>
+            </div>
+
+            <div className="menu-column">
+              <h2>Collections</h2>
+              <ul>
+                <li><a href="#">澄語之藍</a></li>
+                <li><a href="#">秘境探光</a></li>
+                <li><a href="#">四季琉波</a></li>
+                <li><a href="#">微醺晶釀</a></li>
+              </ul>
+            </div>
+
+            <div className="menu-column">
+              <h2>Customized</h2>
+              <div className="menu-child">
+                <ul>
+                  <li><a href="../Jasmine/numtest/numtest-1.html">生命靈數測驗</a></li>
+                  <li><a href="../阿啾/html/customization-1.html">開始客製</a></li>
+                </ul>
+              </div>
             </div>
           </div>
 
@@ -60,14 +70,15 @@ export default function MenuOverlay() {
                 <li>收藏清單</li>
               </ul>
             </div>
-            <div className="menu-last2">
-              <h3>FOLLOW US</h3>
-              <span className="scicon-box">
-                <div className="line"></div>
-                <div className="ig"></div>
-                <div className="fb"></div>
-              </span>
-            </div>
+
+          </div>
+          <div className="menu-footer">
+            <h3>FOLLOW US</h3>
+            <span className="scicon-box">
+              <div className="line"></div>
+              <div className="ig"></div>
+              <div className="fb"></div>
+            </span>
           </div>
         </nav>
       </div>
