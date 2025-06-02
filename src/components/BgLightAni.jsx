2cb2e64de951a1c2cdef6f0eb6c914f60c14fabb
ktlinherstudio <catdogorz520@gmail.com>
2025-06-02 16:46:32 +0800

    5454

--- src/components/BgLightAni.jsx
+++ src/components/BgLightAni.jsx
@@ -3,14 +3,14 @@ import './BgLight.css';
 import './BgLightAni.css';
 
 const crystals = [
-    { src: '/images/Numtest/numtest_bg/ani-cry1.png', className: 'ani-crystal crystal1' },
-    { src: '/images/Numtest/numtest_bg/ani-cry2.png', className: 'ani-crystal crystal2' },
-    { src: '/images/Numtest/numtest_bg/ani-cry3.png', className: 'ani-crystal crystal3' },
-    { src: '/images/Numtest/numtest_bg/ani-cry4.png', className: 'ani-crystal crystal4' },
-    { src: '/images/Numtest/numtest_bg/ani-cry5.png', className: 'ani-crystal crystal5' },
-    { src: '/images/Numtest/numtest_bg/ani-cry6.png', className: 'ani-crystal crystal6' },
-    { src: '/images/Numtest/numtest_bg/ani-cry7.png', className: 'ani-crystal crystal7' },
-    { src: '/images/Numtest/numtest_bg/ani-cry8.png', className: 'ani-crystal crystal8' },
+    { src: './images/Numtest/numtest_bg/ani-cry1.png', className: 'ani-crystal crystal1' },
+    { src: './images/Numtest/numtest_bg/ani-cry2.png', className: 'ani-crystal crystal2' },
+    { src: './images/Numtest/numtest_bg/ani-cry3.png', className: 'ani-crystal crystal3' },
+    { src: './images/Numtest/numtest_bg/ani-cry4.png', className: 'ani-crystal crystal4' },
+    { src: './images/Numtest/numtest_bg/ani-cry5.png', className: 'ani-crystal crystal5' },
+    { src: './images/Numtest/numtest_bg/ani-cry6.png', className: 'ani-crystal crystal6' },
+    { src: './images/Numtest/numtest_bg/ani-cry7.png', className: 'ani-crystal crystal7' },
+    { src: './images/Numtest/numtest_bg/ani-cry8.png', className: 'ani-crystal crystal8' },
 ];
 
 export default function BgLightAni() {
