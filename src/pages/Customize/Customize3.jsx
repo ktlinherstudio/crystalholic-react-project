8a18d315ddac9c86e04b3edc3a03ea0c537ff159
ktlinherstudio <catdogorz520@gmail.com>
2025-06-05 16:06:15 +0800

    54

--- src/pages/Customize/Customize3.jsx
+++ src/pages/Customize/Customize3.jsx
@@ -51,7 +51,8 @@ const handleStartWithRecommend = () => {
    sessionStorage.setItem('designMode', 'recommend'); 
 
    // 預設金屬珠為 ball3
-  sessionStorage.setItem('recommendedMetalImage', './images/Custom/ball3.png');
+  const metalImage = `${import.meta.env.BASE_URL}images/Custom/ball3.png`;
+sessionStorage.setItem('recommendedMetalImage', metalImage);
 
   navigate('/Customize4');
 };
