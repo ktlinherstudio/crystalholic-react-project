a87e0041fa6ab2d48d670ff60eb20beb2387777f
ktlinktlinherstudio <catdogorz520@gmail.com>
2025-06-09 11:17:33 +0800

    ㄉ123

--- src/pages/NumTest/NumTest3.jsx
+++ src/pages/NumTest/NumTest3.jsx
@@ -155,7 +155,7 @@ export default function NumTest3() {
 
                         <div id="button-box">
                             <button className="button1-j">分享測驗結果</button>
-                            <button className="button2-j" onClick={() => navigate('/Customize1')}>
+                            <button className="button2-j" onClick={() => navigate('/Customize3')}>
                                 ✦ 訂製專屬手鍊 ✦
                             </button>
                         </div>
